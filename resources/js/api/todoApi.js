7e7cb77271ce0af016868e263ae44d94052c27be
Justine_FlowerStore <justine.llamera@flowerstore.ph>
2026-09-01 17:05:39 +0800

    checkpoint 3

---
+++ resources/js/api/todoApi.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+// To be changed
+export const putTodo = async () => {
+    // 
+    try {
+        
+    } catch (err) {
+
+    }
+    
+}
+
+
+export const getTodo = async () => {
+    // Feed ID
+    try {
+        const response = await ("")
+    } catch (err) {
+
+    }
+}
+
+export const deleteTodo = async () => {
+    
+    try {
+
+    } catch (err) {
+
+    }
+}
--- resources/js/pages/Main.vue
+++ resources/js/pages/Main.vue
@@ -1,9 +1,77 @@
 <script setup>
 import TodoField from '../components/TodoField.vue';
+import { ref, watchEffect, watch } from 'vue';
+
+
+const createTodo = ref('');
+const searchByKeyword = ref('');
+
+
+const TODO_STATUS = ["Done", "Pending"];
+
+watchEffect(() => {
+    console.log(createTodo.value);
+});
+
+const onSubmit = () => {
+    
+};
+
 </script>
 
 <template>
-    <div class="">
-        
+    <div class="h-min-auto">
+        <div class="w-2xl bg-stone-400 rounded-2xl p-8">
+            <div class="flex flex-col ">
+
+                <!-- Keyword Search -->
+                <div class="w-full">
+                    <h1>Search by Keyword</h1>
+                    <input
+                        v-model="searchByKeyword"
+                        placeholder="Keyword Search"
+                        class="rounded-xl p-2 bg-stone-200 mt-2"
+                    >
+                </div>
+            </div>
+
+            <!-- Create To do -->
+            <div class="mt-6 flex ">
+                <div class="w-full">
+                    <h1>Create Todos</h1>
+                    <input
+                        v-model="createTodo"  
+                        placeholder="Create your todos"
+                        class="bg-stone-200 rounded-xl p-2 w-full mt-2"
+                    >
+                </div>
+            </div>
+
+            <div class="flex justify-end mt-2">
+                <button
+                    class="p-2 bg-stone-200 rounded-md hover:bg-stone-300"
+                    type="submit"
+                >
+                    Submit
+                </button>
+            </div>
+
+            <!-- Result -->
+            <div class="p-2 border-2 border-stone-600 rounded-xl mt-4 bg-stone-100">
+                <div class="bg-stone-200 rounded-md p-2 flex">
+
+                    <div class="w-[70%]">
+                        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        {{  }}
+                    </div>
+
+                    <!-- Status -->
+                    <div>
+                        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                        Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
