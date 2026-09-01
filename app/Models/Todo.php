8e280335d1912874e9fddd004b8b46edfa95d379
Justine_FlowerStore <justine.llamera@flowerstore.ph>
2026-09-01 17:48:02 +0800

    checkpoint 5

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Todo extends Model
 {
     //
+    protected $table = 'todos_table';
+
+    protected $fillable = ['user_id', 'todo', 'status'];
+
+    
 }
